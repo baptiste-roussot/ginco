8b60241fb011fe6d65b6abfb3e71e7f5e78d3323
DERIGENT David (Externe - sous-traitant SSL) <david.derigent@sword-group.com>
2020-03-16 15:33:20 +0100

    Revert "Merge branch 'ajout-broader-from-master' into 'master'"
    This reverts merge request !2

--- ginco-api/src/main/java/fr/mcc/ginco/solr/SearchResult.java
+++ ginco-api/src/main/java/fr/mcc/ginco/solr/SearchResult.java
@@ -49,15 +49,6 @@ public class SearchResult {
     private Integer status;
     private String conceptId;
 	private List<String> languages;
-	private List<String> broaders;
-
-	public List<String> getBroaders() {
-		return broaders;
-	}
-
-	public void setBroaders(List<String> broaders) {
-		this.broaders = broaders;
-	}
 
 	public String getIdentifier() {
 		return identifier;
